c8f6dbe8f604659dda9c63473d5dab8be52ef4a0
Aleksandr <36114404+AlexanderSergeevich1985@users.noreply.github.com>
2018-05-29 11:50:51 +0300

    Update Authentification.java

--- src/uav/Authentification/Authentification.java
+++ src/uav/Authentification/Authentification.java
@@ -9,6 +9,10 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Authentification {
 }
 
@@ -25,6 +29,29 @@ class UserInfo {
     public void setEmail(String email_) { email = email_; }
     public String getTelephone() { return telephone; }
     public void setTelephone(String telephone_) { email = telephone_; }
+    public Date getBirthday() {
+        SimpleDateFormat sdfr = new SimpleDateFormat("dd/MMM/yyyy");
+        Date birth_date = null;
+        try {
+            birth_date = sdfr.parse(birthday);
+        }
+        catch(Exception ex) {
+            System.out.println(ex);
+            return null;
+        }
+        return birth_date;
+    }
+    public boolean setBirthday(Date birthday_) {
+        SimpleDateFormat sdfr = new SimpleDateFormat("dd/MMM/yyyy");
+        try {
+            birthday = sdfr.format(birthday_);
+        }
+        catch(Exception ex) {
+            System.out.println(ex);
+            return false;
+        }
+        return true;
+    }
     @Id
     @Column(name = "ID")
     @GeneratedValue
@@ -37,4 +64,6 @@ class UserInfo {
     private String email;
     @Column(name = "TELEPHONE")
     private String telephone;
+    @Column(name = "BIRTHDAY")
+    private String birthday;
 }
