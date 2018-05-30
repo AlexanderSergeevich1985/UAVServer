0eeaa4026e52d126f28b1eb040a20c3f72c3620a
Aleksandr <36114404+AlexanderSergeevich1985@users.noreply.github.com>
2018-05-30 13:39:27 +0300

    Update RouterTable.java

--- src/uav/GridComputation/RouterTable.java
+++ src/uav/GridComputation/RouterTable.java
@@ -1,13 +1,15 @@
 package uav.GridComputation;
 
 import java.net.InetAddress;
+import java.util.List;
+import java.util.Optional;
 import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicReferenceFieldUpdater;
+import java.util.stream.Collectors;
 
 /**
  * Created by administrator on 29.05.18.
  */
-
 class RouteDesc {
     static <T> String convert_toString(T address) {
         String str_address = null;
@@ -24,55 +26,55 @@ class RouteDesc {
         return str_address;
     }
 
-    <T> boolean setNetwork_destination(T address) {
+    public <T> boolean setNetwork_destination(T address) {
         String str_address = convert_toString(address);
         if (str_address == null) return false;
         network_destination = str_address;
         return true;
     }
 
-    String getNetwork_destination() {
+    public String getNetwork_destination() {
         return network_destination;
     }
 
-    <T> boolean setNetmask(T address) {
+    public <T> boolean setNetmask(T address) {
         String str_address = convert_toString(address);
         if (str_address == null) return false;
         netmask = str_address;
         return true;
     }
 
-    String getNetmask() {
+    public String getNetmask() {
         return netmask;
     }
 
-    <T> boolean setGateway(T address) {
+    public <T> boolean setGateway(T address) {
         String str_address = convert_toString(address);
         if (str_address == null) return false;
         gateway = str_address;
         return true;
     }
 
-    String getGateway() {
+    public String getGateway() {
         return gateway;
     }
 
-    <T> boolean setRinterface(T address) {
+    public <T> boolean setRinterface(T address) {
         String str_address = convert_toString(address);
         if (str_address == null) return false;
         rinterface = str_address;
         return true;
     }
 
-    String getRinterface() {
+    public String getRinterface() {
         return rinterface;
     }
 
-    void setMetric(int metric_) {
+    public void setMetric(int metric_) {
         metric = metric_;
     }
 
-    Integer getMetric() {
+    public Integer getMetric() {
         return metric;
     }
 
@@ -97,6 +99,34 @@ class SharedRouteDesc {
             AtomicReferenceFieldUpdater.newUpdater(SharedRouteDesc.class, RouteDesc.class, "item");
 }
 
-public class RouterTable<T> {
-    ConcurrentSkipListMap<String, T> routes;
+
+public class RouterTable {
+    ConcurrentSkipListMap<String, RouteDesc> routes;
+
+    public void addRoute(String key, RouteDesc desc) {
+        routes.put(key, desc);
+    }
+    public RouteDesc getDescByKey(String key) {
+        return routes.get(key);
+    }
+    public List<RouteDesc> findDescByMetric(int first_metric, Optional<Integer> optParam) {
+        List<RouteDesc> RoutDescs = null;
+        if(!optParam.isPresent()) {
+            RoutDescs = routes.values().parallelStream()
+                    .filter((item) -> {
+                        return item.getMetric() == first_metric ? true : false;
+                    })
+                    .collect(Collectors.toList());
+        }
+        else {
+            Integer second_metric = optParam.get();
+            RoutDescs = routes.values().parallelStream()
+                    .filter((item) -> {
+                        Integer metric = item.getMetric();
+                        return (metric >= first_metric && metric <= second_metric) ? true : false;
+                    })
+                    .collect(Collectors.toList());
+        }
+        return RoutDescs;
+    }
 }
