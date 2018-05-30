38a1be06f818e3f8936d4ca8ea45581166d8377c
Aleksandr <36114404+AlexanderSergeevich1985@users.noreply.github.com>
2018-05-30 16:56:38 +0300

    Add files via upload

---
+++ src/uav/Authentification/UserInfoDAO.java
@@ -0,0 +1,15 @@
+package uav.Authentification;
+
+import java.util.List;
+
+/**
+ * Created by administrator on 30.05.18.
+ */
+
+public interface UserInfoDAO {
+    public void addContact(UserInfo contact);
+    public List<UserInfo> listContact();
+    public void removeContact(Integer id);
+    public UserInfo retriveUserInfo(Integer id);
+    public List<UserInfo> findUsersByTelephone(String telephone);
+}
---
+++ src/uav/Authentification/UserInfoDAOImpl.java
@@ -0,0 +1,72 @@
+package uav.Authentification;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import org.hibernate.SessionFactory;
+
+import java.util.List;
+
+/**
+ * Created by administrator on 23.06.17.
+ */
+
+@Repository
+public class UserInfoDAOImpl implements UserInfoDAO {
+    @Autowired
+    private SessionFactory sessionFactory;
+    public void addContact(UserInfo contact) {
+        try {
+            sessionFactory.getCurrentSession().save(contact);
+        }
+        catch(HibernateException ex) {
+            ex.printStackTrace();
+        }
+    }
+    @SuppressWarnings("unchecked")
+    public List<UserInfo> listContact() {
+        try {
+            Query query = sessionFactory.getCurrentSession().createQuery("from UserInfo");
+            return query.list();
+        }
+        catch(HibernateException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+    }
+    public void removeContact(Integer id) {
+        try {
+            UserInfo contact = (UserInfo) sessionFactory.getCurrentSession().load(UserInfo.class, id);
+            if (null != contact) {
+                sessionFactory.getCurrentSession().delete(contact);
+            }
+        }
+        catch(HibernateException ex) {
+            ex.printStackTrace();
+        }
+    }
+    public UserInfo retriveUserInfo(Integer id) {
+        try {
+            Query query = sessionFactory.getCurrentSession().createQuery("from UserInfo where id = :id");
+            query.setInteger("id", id);
+            return (UserInfo)query.uniqueResult();
+        }
+        catch(HibernateException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+    }
+    public List<UserInfo> findUsersByTelephone(String telephone) {
+        try {
+            Query query = sessionFactory.getCurrentSession().getNamedQuery(UserInfo.NamedQuery.USER_FIND_BY_Phone);
+            query.setString("telephone", "%" + telephone + "%");
+            return (List<UserInfo>)query.list();
+        }
+        catch(HibernateException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+    }
+}
