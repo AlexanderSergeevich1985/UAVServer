2b4740b81ae9846d715998701aae3c3a1957b2c8
Aleksandr <36114404+AlexanderSergeevich1985@users.noreply.github.com>
2018-06-04 17:05:37 +0300

    Create UserProfileType.java

---
+++ src/uav/Authentification/DataModel/UserProfileType.java
@@ -0,0 +1,24 @@
+package uav.Authentification.DataModel;
+
+import java.io.Serializable;
+
+/**
+ * Created by administrator on 04.06.18.
+ */
+
+public enum UserProfileType implements Serializable {
+    USER("USER"),
+    DBA("DBA"),
+    ADMIN("ADMIN");
+
+    String userProfileType;
+
+    private UserProfileType(String userProfileType) {
+        this.userProfileType = userProfileType;
+    }
+
+    public String getUserProfileType() {
+        return userProfileType;
+    }
+
+}
