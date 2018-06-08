e5f7254c776a092c6486d2f58424e49cdf134fae
Aleksandr <36114404+AlexanderSergeevich1985@users.noreply.github.com>
2018-06-08 15:40:25 +0300

    Update KafkaMsgSerializer.java

--- src/uav/KafkaIntegration/KafkaMsgSerializer.java
+++ src/uav/KafkaIntegration/KafkaMsgSerializer.java
@@ -1,3 +1,21 @@
+/**The MIT License (MIT)
+Copyright (c) 2018 by AleksanderSergeevich
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
 package uav.KafkaIntegration;
 
 import com.esotericsoftware.kryo.Kryo;
@@ -8,9 +26,6 @@ import java.lang.reflect.Field;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-/**
- * Created by administrator on 08.06.18.
- */
 public class KafkaMsgSerializer extends com.esotericsoftware.kryo.Serializer<KafkaMsg> {
     private static Logger logger = Logger.getLogger(KafkaMsgSerializer.class.getName());
     private KafkaMsgBuilder kafkaMsgBuilder = new KafkaMsgBuilder();
@@ -19,7 +34,7 @@ public class KafkaMsgSerializer extends com.esotericsoftware.kryo.Serializer<Kaf
         try {
             kafkaMsgBuilder.setMsg(kafkaMsg);
             if(kafkaMsgBuilder.hasNext()) {
-                Field filedClass = kafkaMsgBuilder.getFiled();
+                Field filedClass = kafkaMsgBuilder.getField();
                 if(filedClass.getClass().isInstance(Boolean.TYPE)) {
                     output.writeBoolean(kafkaMsgBuilder.getNext());
                 }
@@ -58,7 +73,7 @@ public class KafkaMsgSerializer extends com.esotericsoftware.kryo.Serializer<Kaf
             kafkaMsg = msgClass.newInstance();
             kafkaMsgBuilder.setMsg(kafkaMsg);
             if(kafkaMsgBuilder.hasNext() && input.available() > 0) {
-                Field filedClass = kafkaMsgBuilder.getFiled();
+                Field filedClass = kafkaMsgBuilder.getField();
                 if(filedClass.getClass().isInstance(Boolean.TYPE)) {
                     kafkaMsgBuilder.setFieldValue(input.readBoolean());
                 }
