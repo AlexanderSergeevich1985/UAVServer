294849a7fc932277a883776873526994327858b6
Aleksandr <36114404+AlexanderSergeevich1985@users.noreply.github.com>
2018-06-12 21:43:01 +0300

    Create UserPrincipalService.java

---
+++ src/uav/Authentification/services/UserPrincipalService.java
@@ -0,0 +1,34 @@
+package uav.Authentification.services;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import uav.Authentification.DataModel.UserPrincipal;
+import uav.Authentification.UserInfo;
+import uav.Authentification.repository.UserInfoRepository;
+
+@Service("userPrincipalService")
+@Transactional
+public class UserPrincipalService implements UserDetailsService {
+    static final Logger logger = LoggerFactory.getLogger(UserPrincipalService.class);
+
+    @Autowired
+    private UserInfoRepository userInfoRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String ssoId) throws UsernameNotFoundException {
+        UserInfo userInfo = userInfoRepository.findByEmail(ssoId);
+        logger.info("User : {}", userInfo);
+        if(userInfo == null) {
+            logger.info("User haven't registered yet");
+            throw new UsernameNotFoundException(ssoId);
+        }
+        return new UserPrincipal(userInfo);
+    }
+}
