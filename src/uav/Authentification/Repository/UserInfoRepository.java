196cfe5c86be8bfc345679b7db80fa00030a1e95
Aleksandr <36114404+AlexanderSergeevich1985@users.noreply.github.com>
2018-06-12 15:04:23 +0300

    Create UserInfoRepository.java

---
+++ src/uav/Authentification/Repository/UserInfoRepository.java
@@ -0,0 +1,9 @@
+package uav.Authentification.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import uav.Authentification.UserInfo;
+
+@Repository
+public interface UserInfoRepository extends JpaRepository<UserInfo, Integer> {
+}
