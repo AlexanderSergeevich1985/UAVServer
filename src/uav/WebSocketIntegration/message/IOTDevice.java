399348b4db30af054a0d4a52f30a95bc05534c9a
Aleksandr <36114404+AlexanderSergeevich1985@users.noreply.github.com>
2018-06-16 02:13:08 +0300

    Add files via upload

---
+++ src/uav/WebSocketIntegration/message/IOTDevice.java
@@ -0,0 +1,53 @@
+package uav.WebSocketIntegration.message;
+
+import org.hibernate.annotations.BatchSize;
+
+import javax.persistence.*;
+import java.util.HashMap;
+import java.util.Map;
+
+@Entity
+@Table(name = "IOTDEVICE")
+public class IOTDevice {
+    @Id
+    @Column(name = "ID")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    @Column(name = "DEVICE_UID", unique = true, nullable = false)
+    private String deviceUID;
+    @Column(name = "DEVICE_TYPE")
+    private String deviceType;
+    @OneToOne(cascade = CascadeType.ALL)
+    @PrimaryKeyJoinColumn(name = "coordinates")
+    private Coordinates coordinates;
+    @ElementCollection(fetch = FetchType.LAZY)
+    @CollectionTable(name="PROPERTIES", joinColumns = @JoinColumn(name = "PROPERTY_ID"))
+    @MapKeyColumn(name="PROPERTY_NAME")
+    @MapKeyClass(String.class)
+    @Column(name="PROPERTY")
+    @BatchSize(size = 20)
+    private Map<String, String> properties = new HashMap<>();
+
+    public void setDeviceUID(String deviceUID) {
+        this.deviceUID = deviceUID;
+    }
+    public String getDeviceUID() {
+        return this.deviceUID;
+    }
+    public void setDeviceType(String deviceType) {
+        this.deviceType = deviceType;
+    }
+    public String getDeviceType() {
+        return this.deviceType;
+    }
+    public void setProperty(String key, String value) {
+        this.properties.put(key, value);
+    }
+    public String getProperty(String key) {
+        return this.properties.get(key);
+    }
+    @Override
+    public String toString() {
+        return "Device [device UID=" + deviceUID + ", device Type=" + deviceType + "]";
+    }
+}
