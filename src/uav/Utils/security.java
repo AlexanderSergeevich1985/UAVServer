57be047f45b328fb594edcc956433f92369971b8
Aleksandr <36114404+AlexanderSergeevich1985@users.noreply.github.com>
2018-06-25 17:04:28 +0300

    Update security.java

--- src/uav/Utils/security.java
+++ src/uav/Utils/security.java
@@ -29,7 +29,7 @@ import java.security.spec.InvalidKeySpecException;
 import java.security.spec.PKCS8EncodedKeySpec;
 import java.security.spec.X509EncodedKeySpec;
 
-public class Utils {
+public class security {
     static public KeyPair generate_RSA() {
         KeyPair keyPair = null;
         try {
