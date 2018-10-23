a729115f15318d2bfeca96b907db3debc795f35f
Александр Викторов <alexandervas1985@outlook.com>
2018-10-24 01:11:59 +0300

    #Adding object state descriptor and transition map between states

---
+++ src/uav/Security/Configuration/ResourceServerConfiguration.java
@@ -0,0 +1,29 @@
+package uav.Security.Configuration;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.error.OAuth2AccessDeniedHandler;
+
+@Configuration
+@EnableResourceServer
+public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter {
+    private static final String RESOURCE_ID = "resource_id";
+
+    @Override
+    public void configure(ResourceServerSecurityConfigurer resources) {
+        resources.resourceId(RESOURCE_ID).stateless(false);
+    }
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.anonymous().disable()
+                .requestMatchers().antMatchers("/apps/exe/**")
+                .and().authorizeRequests()
+                .antMatchers("/apps/control/**").access("hasRole('ADMIN')")
+                .and().exceptionHandling().accessDeniedHandler(new OAuth2AccessDeniedHandler());
+    }
+}
