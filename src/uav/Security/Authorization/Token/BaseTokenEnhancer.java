e741bbeec28db4f08ceaece2cc2a51cb12503995
Александр Викторов <alexandervas1985@outlook.com>
2018-10-28 16:03:03 +0300

    #Adding custom token enhancer

--- src/uav/Security/Authorization/Token/BaseTokenEnhancer.java
+++ src/uav/Security/Authorization/Token/BaseTokenEnhancer.java
@@ -5,9 +5,11 @@ import org.springframework.security.oauth2.common.DefaultOAuth2AccessToken;
 import org.springframework.security.oauth2.common.OAuth2AccessToken;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.security.oauth2.provider.token.TokenEnhancer;
+import org.springframework.stereotype.Component;
 
 import java.util.Map;
 
+@Component
 public class BaseTokenEnhancer implements TokenEnhancer {
     @Override
     public OAuth2AccessToken enhance(OAuth2AccessToken accessToken, OAuth2Authentication authentication) {
