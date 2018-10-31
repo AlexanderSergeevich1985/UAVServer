e05021a2442a226352040564d5c1c31e81d5bf9e
Александр Викторов <alexandervas1985@outlook.com>
2018-10-31 03:37:01 +0300

    #Adding a new base operation

--- src/uav/BaseOperation/NeuronFunction.java
+++ src/uav/BaseOperation/NeuronFunction.java
@@ -18,42 +18,40 @@
  */
 package uav.BaseOperation;
 
-import org.apache.spark.mllib.linalg.BLAS;
-import org.apache.spark.mllib.linalg.Vectors;
 import org.apache.spark.mllib.linalg.DenseVector;
 import org.apache.spark.mllib.linalg.Vector;
+import org.apache.spark.mllib.linalg.BLAS;
 import uav.Utils.DateTimeHelper;
 
 import java.util.concurrent.TimeUnit;
 
-public class PerseptronFunctionM {
+public class NeuronFunction {
     static final int DEFAULT_VECTOR_SIZE = 10;
 
     private Vector weightCoeff;
-    private Vector prevInputSignal;
     private double outSignalValue;
-    private double delayCoeff;
     public double scalefactor = 60;
     public static double smoothFactor = 0.9; // 0.5 < smoothFactor < 0.9
     private DateTimeHelper.StopWatch timer = DateTimeHelper.StopWatch.getInstance();
+    //long updateTime = System.currentTimeMillis();
 
-    public PerseptronFunctionM(double[] weightCoeffArray) {
+    public NeuronFunction(double[] weightCoeffArray) {
         weightCoeff = new DenseVector(weightCoeffArray);
-        prevInputSignal = Vectors.zeros(weightCoeffArray.length);
     }
 
-    double calcDelayCoeff() {
+    double weightSignal(double[] signalArray) {
+        return BLAS.dot(weightCoeff, (new DenseVector(signalArray)));
+    }
+
+    //delay rest value
+    double calcDelayRestValue() {
+        //long eventTime = System.currentTimeMillis();
+        //long timeElapsed = eventTime - updateTime;
+        //this.updateTime = eventTime;
         timer.stop();
         long timeElapsed = timer.getTimeMeasurement(TimeUnit.MILLISECONDS);
         timer.reset();
-        return Math.exp(-1 * timeElapsed/scalefactor);
-    }
-
-    Vector calcDiffInputSignal(double[] signalArray) {
-        Vector result = new DenseVector(signalArray);
-        BLAS.axpy((-1) * delayCoeff, prevInputSignal, result);
-        prevInputSignal = new DenseVector(signalArray);
-        return result;
+        return outSignalValue * Math.exp(-1 * timeElapsed/scalefactor);
     }
 
     double calcActivateFunction(double outSignalValue) {
@@ -63,10 +61,9 @@ public class PerseptronFunctionM {
             return 0.001 * outSignalValue;
     }
 
-    public double updateState(double[] inSignalArray) {
-        this.delayCoeff = calcDelayCoeff();
-        double newOutSignalValue = BLAS.dot(weightCoeff, calcDiffInputSignal(inSignalArray));
-        double delayRestValue = delayCoeff * outSignalValue;
+    public double updateState(double[] signalArray) {
+        double newOutSignalValue = weightSignal(signalArray);
+        double delayRestValue = calcDelayRestValue();
         this.outSignalValue = smoothFactor * (newOutSignalValue - delayRestValue) + (1 - smoothFactor) * delayRestValue;
         return calcActivateFunction(this.outSignalValue);
     }
--- src/uav/BaseOperation/NeuronFunctionM.java
+++ src/uav/BaseOperation/NeuronFunctionM.java
@@ -18,40 +18,42 @@
  */
 package uav.BaseOperation;
 
+import org.apache.spark.mllib.linalg.BLAS;
+import org.apache.spark.mllib.linalg.Vectors;
 import org.apache.spark.mllib.linalg.DenseVector;
 import org.apache.spark.mllib.linalg.Vector;
-import org.apache.spark.mllib.linalg.BLAS;
 import uav.Utils.DateTimeHelper;
 
 import java.util.concurrent.TimeUnit;
 
-public class PerseptronFunction {
+public class NeuronFunctionM {
     static final int DEFAULT_VECTOR_SIZE = 10;
 
     private Vector weightCoeff;
+    private Vector prevInputSignal;
     private double outSignalValue;
+    private double delayCoeff;
     public double scalefactor = 60;
     public static double smoothFactor = 0.9; // 0.5 < smoothFactor < 0.9
     private DateTimeHelper.StopWatch timer = DateTimeHelper.StopWatch.getInstance();
-    //long updateTime = System.currentTimeMillis();
 
-    public PerseptronFunction(double[] weightCoeffArray) {
+    public NeuronFunctionM(double[] weightCoeffArray) {
         weightCoeff = new DenseVector(weightCoeffArray);
+        prevInputSignal = Vectors.zeros(weightCoeffArray.length);
     }
 
-    double weightSignal(double[] signalArray) {
-        return BLAS.dot(weightCoeff, (new DenseVector(signalArray)));
-    }
-
-    //delay rest value
-    double calcDelayRestValue() {
-        //long eventTime = System.currentTimeMillis();
-        //long timeElapsed = eventTime - updateTime;
-        //this.updateTime = eventTime;
+    double calcDelayCoeff() {
         timer.stop();
         long timeElapsed = timer.getTimeMeasurement(TimeUnit.MILLISECONDS);
         timer.reset();
-        return outSignalValue * Math.exp(-1 * timeElapsed/scalefactor);
+        return Math.exp(-1 * timeElapsed/scalefactor);
+    }
+
+    Vector calcDiffInputSignal(double[] signalArray) {
+        Vector result = new DenseVector(signalArray);
+        BLAS.axpy((-1) * delayCoeff, prevInputSignal, result);
+        prevInputSignal = new DenseVector(signalArray);
+        return result;
     }
 
     double calcActivateFunction(double outSignalValue) {
@@ -61,9 +63,10 @@ public class PerseptronFunction {
             return 0.001 * outSignalValue;
     }
 
-    public double updateState(double[] signalArray) {
-        double newOutSignalValue = weightSignal(signalArray);
-        double delayRestValue = calcDelayRestValue();
+    public double updateState(double[] inSignalArray) {
+        this.delayCoeff = calcDelayCoeff();
+        double newOutSignalValue = BLAS.dot(weightCoeff, calcDiffInputSignal(inSignalArray));
+        double delayRestValue = delayCoeff * outSignalValue;
         this.outSignalValue = smoothFactor * (newOutSignalValue - delayRestValue) + (1 - smoothFactor) * delayRestValue;
         return calcActivateFunction(this.outSignalValue);
     }
