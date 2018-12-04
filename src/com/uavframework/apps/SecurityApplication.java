1f240cdf18bc32f394d5fd1340b1660217102c14
Александр Викторов <alexandervas1985@outlook.com>
2018-12-04 20:25:20 +0300

    #Adding spring config support

--- src/com/uavframework/apps/SecurityApplication.java
+++ src/com/uavframework/apps/SecurityApplication.java
@@ -3,12 +3,14 @@ package com.uavframework.apps;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
 @EnableResourceServer
 @EnableDiscoveryClient
+@EnableConfigServer
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityApplication {
 
